ab635885f04a263872499d525289c4790d4fdf1a
Joshua Peek <josh@joshpeek.com>
2010-01-31 13:14:09 -0600

    Revert "Now a plugin"
    This reverts commit acaf71c844c826255bc0e8387651b593cdc84040.

--- init.rb
+++
@@ -1,1 +0,0 @@
-require 'prototype_helper'
--- test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'test/unit'
-require 'active_support'
-require 'action_view'
-require 'action_controller'
-require 'action_view/test_case'
-
-
